30457e896f2a6c0be9d5a320debe140c62222025
LivinkumarStackly <livinkumar@thestackly.com>
2026-04-06 11:09:52 +0530

    Variables, Datatypes and TypeCasting

---
+++ VarDatatypeCasting.py
@@ -0,0 +1,36 @@
+# a=12
+# a=45
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+
+# input1=10
+
+# print(input)
+
+# firstName= "Livin"
+# a= "Livin"
+# print(A)
+
+
+int1=12
+flt1=11.11
+str1="Hello Guys!"
+str2="Hi..!"
+# print(type(str1))
+
+# print(12+12)
+# print(str1+12)
+
+name="Vikram"
+age=57
+
+# His name is Vikram. He is 57 years old.
+
+# print("His name is "+name+". He is "+str(age)+" years old.")
+
+# a=True 
+# b=False 
+
+# print(type(a))
--- intro.py
+++ intro.py
@@ -4,9 +4,14 @@ print(56)
 print(21)'''
 
 #these lines are used to print out some data
-print(56)
-print(21)
+# print(56)
+# print(21)
 
 
-print(56)
-print("Hello World")
+# print(56)
+# print("Hello World")
+
+# a=input("Enter Your Name: ")
+# print(a)
+# print(a)
+# print(a)
