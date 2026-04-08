22f22a177bdbc02e5100151ee1307ef092fba00c
LivinkumarStackly <livinkumar@thestackly.com>
2026-04-08 11:39:25 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,69 @@
+# a=3
+# b=5
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(20**0)
+
+# print(3+4-16*32/8)
+
+# Comparison
+
+# a=10
+# b=10
+
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>=b)
+
+# print("a">"b") #ASCII
+
+
+# Logical 
+
+# a= 67 
+# b= 76 
+
+# print(a and b)
+# print(a or b)
+
+# print(not not a)
+# print(not True)
+
+# print((a-3>b-20) and ((a>b) or not (b**2 > a*20)))
+
+# print(not (b**2 > a*20))
+
+# num=10
+# num+=10 #20
+# num-=15 #5
+# num*=10 #50
+# num/=2 #25
+# num//=3 #8
+# num%=3 # 2
+# num**=4 #16
+
+# print(num)
+
+# print(13&33)
+# print(13|33)
+# print(13^33)
+
+# print(22>>3)
+# print(56<<3)
+
+# 100001
+# 001101
+# ------
+# 101100
+
+# 10110|
+# 111000000
+
+# print(~-4)
+# print(~45)
