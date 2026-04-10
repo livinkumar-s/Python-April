47f9fd6ed36014494e83f3a5bd45681be97a7042
LivinkumarStackly <livinkumar@thestackly.com>
2026-04-10 11:20:16 +0530

    Conditional Clauses

---
+++ ControlFlow.py
@@ -0,0 +1,94 @@
+# if False:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+# else:
+#     print("Step4")
+#     print("Step5")
+
+# for i in range(20):
+#     print("Step4")
+#     print("Step5")
+#     print("Step4")
+# print("Step5")
+
+# Conditional Clause
+
+# age= int(input("Enter your age: "))
+
+# if age>=18:
+#     print("You can vote") # if he is 18 or older
+# else:
+#     print("You cannot vote...!")
+
+# a=10
+
+# if a<9:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+# else:
+#     print("From Else Block")
+
+# print("Final Step")
+
+# a=10
+
+# if a>11:
+#     print("From If block")
+# elif a==5:
+#     print("From Elif1 Block")
+# elif a<5:
+#     print("From Elif2 Block")
+# elif a<=5:
+#     print("From Elif3 Block")
+# else:
+#     print("From Else Block")
+
+# gender="Female"
+# balance=1000
+
+# male+ 5000 --> offer 
+# female + 5000 --> offer+loan
+# male+ <5000 --> nothing
+# female+ <5000 --> offer
+
+# if gender=="male":
+#     if balance>=5000:
+#         print("Offer")
+#     else:
+#         print("Nothing")
+# else:
+#     if balance>=5000:
+#         print("Offer + Loan")
+#     else:
+#         print("Offer")
+
+# marks = int(input("Enter your marks: "))
+# if marks >= 90 and marks <= 100:
+#     print("Grade: O")
+# elif marks >= 80 and marks <= 89:
+#     print("Grade: A+")
+# elif marks >= 70 and marks <= 79:
+#     print("Grade: A")
+# elif marks >= 60 and marks <= 69:
+#     print("Grade: B")
+# elif marks >= 50 and marks <= 59:
+#     print("Grade: C")
+# elif marks>=0 and marks < 50:
+#     print("Result: Fail")
+# else:
+#     print("Invalid marks")
+
+# x=15
+
+# if x>10:
+#     if x>15:
+#         print("Very High")
+#     else:
+#         print("High")
+# else:
+#     if x<5:
+#         print("Very low")
+#     else:
+#         print("Low")
