5cba221e5bc9ec66a54128dcedb2ca90b4c2fa86
LivinkumarStackly <livinkumar@thestackly.com>
2026-04-17 11:42:13 +0530

    Functions and Scope

---
+++ Functions.py
@@ -0,0 +1,100 @@
+# def printStep():
+#     print("Step1")
+#     print("Step2")
+#     return
+#     print("Step3")
+
+# a=printStep()
+# print(a)
+
+# printStep()
+# printStep()
+# printStep()
+# printStep()
+
+# input()
+# print()
+# int()
+
+# def addNums(a,b):
+#     print(a+b)
+
+# addNums(10,10)
+# addNums(10,100)
+
+# def greet(n1,n2):
+#     print("Good Morning "+n1+".")
+#     print("Hello "+n2)
+
+
+# greet(n2="Sam",n1="Tom")
+# print("Hello",end="")
+
+# def mulNum(a,b,c=10):
+#     print(a*b*c)
+
+# mulNum(2,3,20)
+
+# def findSum(*a):
+#     print(sum(a))
+
+# findSum(423,234,423,234,234,234,24,2)
+
+# def fetchDetails(**a):
+#     print(a)
+
+# fetchDetails(name="Ken",age=45,height=180)
+
+
+
+# def mulVals(a,b):
+#     return a*b
+
+# print(mulVals(5,mulVals(7,mulVals(2,mulVals(3,1)))))
+
+# lis1=[1,2,3,4,5]
+
+# lis1.reverse()
+
+# print(lis1[::-1])
+
+# l1=[1,2,3,2,1]
+
+# if l1 == l1.reverse():
+#     print("Pal")
+# else:
+#     print("No Pal")
+
+# a=10 # global. accessible anywhere
+
+# def func1():
+#     a="Hello"
+#     print(a)
+
+# def func2():
+#     print(a)
+
+# func1()
+# func2()
+# print(a)
+
+# a=10
+
+# def inc():
+#     global a
+#     a=a+1
+#     print(a)
+
+# inc()
+# inc()
+# inc()
+# inc()
+# inc()
+
+size = int(input("Enter the size: "))
+
+print("* * * * * ")
+print("*       * ")
+print("*       * ")
+print("*       * ")
+print("* * * * * ")
